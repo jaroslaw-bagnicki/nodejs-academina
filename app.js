7f60349e632d04b700dbfadae03d21cf1b9c3622
Jarosław Bagnicki <jarek@bagnicki.net>
2019-12-22 10:39:13 +0100

    Add sample form

--- app.js
+++ app.js
@@ -4,9 +4,27 @@ const server = http.createServer((req, res) => {
 
     const { url, method, headers } = req;
     console.log({ url, method, headers });
+
+    if ( url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head>');
+        res.write('<title>Hello World Page</title>');
+        res.write('</head>');
+        res.write('<body>');
+        res.write(`
+            <form action="/send-message" method="POST">
+                <input name="message" type="text"/>
+                <button type="submit">Submit</button>
+            </form>
+        `);
+        res.write('</body>');
+        res.write('</html');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head>');
+    res.write('<head>'); 
     res.write('<title>Hello World Page</title>');
     res.write('</head>');
     res.write('<body>');
