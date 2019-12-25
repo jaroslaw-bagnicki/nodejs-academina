dc5b08e20372b2e9c238225846f89cb6070c0b13
Jarosław Bagnicki <jarek@bagnicki.net>
2019-12-25 12:12:31 +0100

    Add handling routes '/' and '/users'

--- 03-task/rootController.js
+++ 03-task/rootController.js
@@ -24,11 +24,37 @@ const rootController = (req, res) => {
     switch(url) {
         case ROUTES.home: {
                 logRequestDetails(req);
-                return res.end();
+                return res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                }).end(`
+                    <html>
+                        <head>
+                            <title>Task #3</title>
+                        </head>
+                        <body>
+                            <h1>Greetings from homepage!</h1>
+                        </body>
+                    </html>
+                `);
             }
         case ROUTES.users: {
             logRequestDetails(req);
-            return res.end();
+            const users = ['user1', 'user2', 'user3'];
+            return res.writeHead(200, {
+                'Content-Type': 'text/html',
+            }).end(`
+                <html>
+                    <head>
+                        <title>Task #3</title>
+                    </head>
+                    <body>
+                        <h2>Users</h2>
+                        <ul>
+                            ${users.map(user => `<li>${user}</li>`).join('')}
+                        </ul>
+                    </body>
+                </html>
+            `);
         }
         default: {
             logRequestDetails(req);
