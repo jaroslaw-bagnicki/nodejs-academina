f9f2460b78839a951039f1e8424ce35c41d2fde6
Jarosław Bagnicki <jarek@bagnicki.net>
2019-12-31 06:28:19 +0100

    Add 404 page template

--- 04-expressjs/src/middleware/notFound.js
+++ 04-expressjs/src/middleware/notFound.js
@@ -1,11 +1,12 @@
+const path = require('path');
+
 /**
  * 
  * @param {import('express').Request} req 
  * @param {import('express').Response} res 
  */
 const notFoundMiddleware = (req, res) => {
-    res.status(404)
-        .send('<h1>Page not found :(</h1>');
+    res.status(404).sendFile(path.resolve('src/views/not-found.html'));
 };
 
 module.exports = notFoundMiddleware;
