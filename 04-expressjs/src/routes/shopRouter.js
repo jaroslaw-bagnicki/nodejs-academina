c1ec4aa4a6506b45bbfa8d589ab9fab916004fab
Jarosław Bagnicki <jarek@bagnicki.net>
2020-01-02 16:52:59 +0100

    Fix looping in handlebars template

--- 04-expressjs/src/routes/shopRouter.js
+++ 04-expressjs/src/routes/shopRouter.js
@@ -8,7 +8,7 @@ shopRouter.route(HOME)
     .get(function getHomePage(req, res) {
         // res.sendFile(path.resolve('src/views/home.html'));
         console.log({ products });
-        res.render('home', { products, title: 'Shop', path: '/' });
+        res.render('home', { products, noProducts: products.length === 0, title: 'Shop', path: '/' });
     });
 
 module.exports = shopRouter;
