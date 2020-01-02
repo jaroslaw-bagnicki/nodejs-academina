41f51c276bb415f8d13413450519233f6b2f1089
Jarosław Bagnicki <jarek@bagnicki.net>
2020-01-02 15:34:12 +0100

    Some fixies

--- 04-expressjs/src/routes/adminRouter.js
+++ 04-expressjs/src/routes/adminRouter.js
@@ -1,12 +1,10 @@
-// const path = require('path');
 const express = require('express');
 
 const adminRouter = express.Router();
 
 adminRouter.route('/admin/add-product')
     .get(function getAddProductPage(req, res) {
-        // res.sendFile(path.resolve('src/views/add-product.html'));
-        res.render('add-product');
+        res.render('add-product', { title: 'Add new product', path: '/admin/add-product'});
     });
 
 module.exports = adminRouter;
