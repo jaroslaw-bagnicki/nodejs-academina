5d8f33f79cf205270635d2137c4e9c3a28498eca
Jarosław Bagnicki <jarek@bagnicki.net>
2020-01-02 15:08:46 +0100

    Refactor not-found page

--- 04-expressjs/src/middleware/notFound.js
+++ 04-expressjs/src/middleware/notFound.js
@@ -1,12 +1,10 @@
-const path = require('path');
-
 /**
  * 
  * @param {import('express').Request} req 
  * @param {import('express').Response} res 
  */
 const notFoundMiddleware = (req, res) => {
-    res.status(404).sendFile(path.resolve('src/views/not-found.html'));
+    res.status(404).render('not-found');
 };
 
 module.exports = notFoundMiddleware;
