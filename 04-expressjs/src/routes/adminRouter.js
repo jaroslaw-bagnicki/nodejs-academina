1deef5fc297266b2ac09d1c992503d8d99492686
Jarosław Bagnicki <jarek@bagnicki.net>
2020-01-02 14:48:09 +0100

    Add new product pug template

--- 04-expressjs/src/routes/adminRouter.js
+++ 04-expressjs/src/routes/adminRouter.js
@@ -1,11 +1,12 @@
-const path = require('path');
+// const path = require('path');
 const express = require('express');
 
 const adminRouter = express.Router();
 
 adminRouter.route('/admin/add-product')
     .get(function getAddProductPage(req, res) {
-        res.sendFile(path.resolve('src/views/add-product.html'));
+        // res.sendFile(path.resolve('src/views/add-product.html'));
+        res.render('add-product');
     });
 
 module.exports = adminRouter;
