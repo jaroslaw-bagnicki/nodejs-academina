bf5f154f94e96f2faca80e11e4c868375dc4233e
Jarosław Bagnicki <jarek@bagnicki.net>
2020-01-03 14:00:50 +0100

    Scaffold app and routing

--- assignments/4/app.js
+++ assignments/4/app.js
@@ -1,1 +1,21 @@
-console.log("hello word");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+
+const app = express();
+
+app.engine('hbs', handlebars({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.render('home', { title: 'Home' }));
+
+app.get('/users', (req, res) => res.render('users', { title: 'Users' }));
+
+app.post('/add-user', (req, res) => {
+    console.log(req.body);
+})
+
+app.listen(3000, () => console.log('Server listen on port 3000...'));
