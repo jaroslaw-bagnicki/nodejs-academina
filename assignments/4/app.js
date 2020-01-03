f9e586ab527b2a69dddbf8d5007dcc53b675b616
Jarosław Bagnicki <jarek@bagnicki.net>
2020-01-03 14:39:49 +0100

    Finish assignment

--- assignments/4/app.js
+++ assignments/4/app.js
@@ -4,6 +4,8 @@ const handlebars = require('express-handlebars');
 
 const app = express();
 
+const users = [];
+
 app.engine('hbs', handlebars({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
@@ -12,10 +14,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('home', { title: 'Home' }));
 
-app.get('/users', (req, res) => res.render('users', { title: 'Users' }));
+app.get('/users', (req, res) => res.render('users', { 
+    title: 'Users',
+    noUser: users.length === 0,
+    users: users,
+}));
 
 app.post('/add-user', (req, res) => {
-    console.log(req.body);
+    if (req.body.username) {
+        users.push({ name: req.body.username })
+        res.redirect('/');
+    } else {
+        res.sendStatus(400);
+    }
 })
 
 app.listen(3000, () => console.log('Server listen on port 3000...'));
